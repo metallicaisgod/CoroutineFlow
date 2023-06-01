f531c50af117120ae434c0226567190d7d504d7b
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-06-01 11:08:52 +0300

    Initial commit

---
+++ app/src/main/java/com/kirillmesh/coroutineflow/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.kirillmesh.coroutineflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kirillmesh.coroutineflow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonUsersActivity.setOnClickListener {
+            startActivity(UsersActivity.newIntent(this))
+        }
+    }
+}
