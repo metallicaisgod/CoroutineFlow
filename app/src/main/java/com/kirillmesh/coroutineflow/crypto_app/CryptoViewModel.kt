17ffb72721c986cce9d57d29ec537baf4c2c6f43
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-06-05 14:47:56 +0300

    Flow on UI repeatOnLifecycle()

--- app/src/main/java/com/kirillmesh/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/kirillmesh/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,16 +1,14 @@
 package com.kirillmesh.coroutineflow.crypto_app
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.*
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    val state: LiveData<State> = repository.getCurrencyList()
+    val state: Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
         .map { State.Content(it) as State }
         .onStart {
@@ -20,8 +18,4 @@ class CryptoViewModel : ViewModel() {
         .onEach {
             Log.d("CryptoViewModel", "OnEach")
         }
-        .onCompletion {
-            Log.d("CryptoViewModel", "onCompletion")
-        }
-        .asLiveData()
 }
